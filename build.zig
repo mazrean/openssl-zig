ef362e08f9992bdf55d5e690cbf6f2c267f113be
mazrean <shunsuke.w04@gmail.com>
2026-03-14 18:00:38 +0900

    fix: add OPENSSL_NO_DEFAULT_THREAD_POOL to providers build

--- build.zig
+++ build.zig
@@ -1052,6 +1052,7 @@ fn libprovider(b: *std.Build, target: std.Build.ResolvedTarget, optimize: std.bu
     lib.root_module.addCMacro("OPENSSL_NO_KTLS", "");
     lib.root_module.addCMacro("OPENSSL_NO_QUIC", "");
     lib.root_module.addCMacro("OPENSSL_NO_THREAD_POOL", "");
+    lib.root_module.addCMacro("OPENSSL_NO_DEFAULT_THREAD_POOL", "");
     lib.root_module.addCMacro("OPENSSL_CPUID_OBJ", "");
     if (lib.rootModuleTarget().os.tag.isDarwin()) {
         lib.root_module.addCMacro("OPENSSL_SYS_MACOSX", "1");
